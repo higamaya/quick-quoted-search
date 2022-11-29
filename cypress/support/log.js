e6f9c82a959748e1c48952c57441f54531ff64a7
Mitsunobu Yoneda <90941863+higamaya@users.noreply.github.com>
2022-11-29 10:09:34 +0900

    test: use `cy.log()` instead of `Cypress.log()` if possible

--- cypress/support/log.js
+++ cypress/support/log.js
@@ -5,16 +5,29 @@ const Types = {
   E: { header: "ERROR", method: "error" },
 };
 
-export function log(type, message, params) {
-  const paramsArray = [];
-  if (params) {
-    for (const [key, value] of Object.entries(params)) {
-      paramsArray.push(`\n${key}=`);
-      paramsArray.push(value);
+Cypress.on("command:start", function (command) {
+  Cypress.qqs ??= {};
+  Cypress.qqs.currentCommand = command;
+});
+
+Cypress.on("command:end", function (_command) {
+  Cypress.qqs.currentCommand = undefined;
+});
+
+export function log(type, message, args) {
+  const argArray = [];
+  if (args) {
+    for (const [key, value] of Object.entries(args)) {
+      argArray.push(`\n${key}=`);
+      argArray.push(value);
     }
   }
   const typeInfo = Types[type];
   assert.exists(typeInfo);
-  console[typeInfo.method]("[TEST]", `[${typeInfo.header}]`, message, ...paramsArray);
-  Cypress.log({ message: `[${typeInfo.header}] ${message}` });
+  console[typeInfo.method]("[TEST]", `[${typeInfo.header}]`, message, ...argArray);
+  if (Cypress.qqs?.currentCommand) {
+    Cypress.log({ message: `[${typeInfo.header}] ${message}` });
+  } else {
+    cy.log(`[${typeInfo.header}] ${message}`, ...argArray);
+  }
 }
